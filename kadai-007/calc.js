528e06dbab46e5d61c83934b289a19a78b990164
150fc208 <150fc208@gmail.com>
2024-09-23 08:45:54 +0900

    Add files via upload

--- kadai-007/calc.js
+++ kadai-007/calc.js
@@ -1,12 +1,9 @@
-const num = 15;
-if (num % 3 === 0 && num % 5 === 0) {
+let num = Math.floor(Math.random() * 20);
+if (num % 3 == 0 && num % 5 == 0) {
     console.log("3と5の倍数です");
-}
-if (num % 3 === 0) {
+} else if (num % 3 == 0) {
     console.log("3の倍数です");
-}
-
-if (num % 5 === 0) {
+} else if (num % 5 == 0) {
     console.log("5の倍数です");
 } else {
     console.log(num);
